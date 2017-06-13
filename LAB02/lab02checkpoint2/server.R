761cb5f00f135b6c887022d78c01a6fe61ed3216
Paulo Vinícius Soares <paulo.soares@ccc.ufcg.edu.br>
2017-06-13 12:48:06 -0300

    Finalização do checkpoint 2.
    Geração de gŕaficos usando shiny e plotly.

--- LAB02/lab02checkpoint2/server.R
+++ LAB02/lab02checkpoint2/server.R
@@ -10,28 +10,15 @@
 library(shiny)
 library(tidyverse)
 library(plotly)
+library(rsconnect)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output, session) {
   
   series_from_imdb <- read_csv("https://raw.githubusercontent.com/nazareno/imdb-series/master/data/series_from_imdb.csv")
   all_series_name <- unique(series_from_imdb$series_name)
-  selected_series_name <- c("Modern Family","The 100","Black Mirror","Sense8")
   
-  dataSeries <- series_from_imdb %>% filter(series_name %in% selected_series_name)
-  
-  #selected_series_name <- c(selected_series_name, "The Simpsons")
-  
-  observeEvent(input$select_series_button, {
-    selected_series_name <- c(selected_series_name, input$select_series)
-    })
-  
-  output$event <- renderPrint({
-    d <- selected_series_name
-    if (is.null(d)) "Hover on a point!" else d
-  })
-  
-  output$distPlot <- renderPlotly({
+  output$line_graph <- renderPlotly({
     
     # Plotagem final dos dados usando plotly
     f <- list(
@@ -39,8 +26,24 @@ shinyServer(function(input, output, session) {
       color = "#7f7f7f"
     )
     
-    plot_ly(data=dataSeries, name="Notas gerais dos episódios", x=~series_name, y=~UserRating) %>%
-      layout(xaxis = list(title = "Nome da série", tickfont=f), yaxis = list(title = "Nota dos episódios"))
+    plot_ly(data=series_from_imdb %>% filter(series_name %in% input$select_series_name), name="Notas gerais dos episódios", x=~series_ep, y=~UserRating) %>%
+      layout(xaxis = list(title = "Número do episódio", tickfont=f), yaxis = list(title = "Nota dos episódios")) %>% 
+      add_lines(hoverinfo = 'text', text=~paste('Episódio: ', season_ep, '<br>Temporada: ', season, '<br> Nome do episódio: ', Episode, '<br> Nota: ', UserRating), color=~as.character(season), showlegends=FALSE)
+  })
+  
+  output$dd <- renderPlotly({
+    
+    # Plotagem final dos dados usando plotly
+    f <- list(
+      size = 10,
+      color = "#7f7f7f"
+    )
+    
+    plot_ly(data=series_from_imdb %>% filter(series_name %in% input$select_series_name), name="Notas gerais dos episódios", x=~series_name, y=~UserRating, type="box", hoverinfo='text') %>%
+      layout(xaxis = list(title = "", tickfont=f), yaxis = list(title = "Nota dos episódios")) %>%
+      add_markers(x=~series_name, y=~median(UserRating), hoverinfo='text', text=~paste('Nota da série: ', median(UserRating)), showlegends=FALSE) %>%
+      add_markers(x=~series_name, y=~max(UserRating), hoverinfo='text', text=~paste('Nota máxima de um episódio: ', max(UserRating)), showlegends=FALSE) %>%
+      add_markers(x=~series_name, y=~min(UserRating), hoverinfo='text', text=~paste('Nota mínima de um episódio: ', min(UserRating)), showlegends=FALSE)
     
   })
   
--- LAB02/lab02checkpoint2/ui.R
+++ LAB02/lab02checkpoint2/ui.R
@@ -8,13 +8,42 @@
 #
 
 library(shiny)
+library(plotly)
+library(tidyverse)
+
+series_from_imdb <- read_csv("https://raw.githubusercontent.com/nazareno/imdb-series/master/data/series_from_imdb.csv")
 
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
-    verbatimTextOutput("event"),
-    plotlyOutput("distPlot"),
-    textInput(inputId = "select_series", label="Digite o nome de uma série para consulta: "),
-    actionButton("select_series_button", "Adicionar série ao gráfico")
+  
+  titlePanel("Desempenho de uma série de acordo com dados do IMDB"),
+  
+  sidebarLayout(
     
-  )
-)
+    
+    sidebarPanel(
+      selectInput("select_series_name", "Nome da série:", 
+                  choices=unique(series_from_imdb$series_name)),
+      helpText("O banco de dados conta com, aproximadamente, 512 séries.")
+    ), 
+    
+    mainPanel(
+      br(),
+      p("Na análise de hoje, eu vou mostrar para vocês o desempenho de várias séries cujas notas foram coletadas a partir
+        do Internet Movie Database (IMDB), que é o site mais popular no quesito avaliação de séries."),
+      p("Para ver o desempenho da sua série favorita, basta procurá-la na lista ao lado e os gráficos serão gerados.
+        Temos dois tipos de gráficos, um de linha e um de caixa. O gráfico de linha é mais intuitivo e se refere às notas
+        em relação a cada episódio. As avaliações por temporadas são divididas por cores também, para facilitar a leitura. O gráfico de caixa, por outro lado, nos mostra informações mais interessantes, como por exemplo,
+        a nota geral da série (baseada na mediana), além da maior e menor nota que um episódio já recebeu na série."),
+      p("Aproveitem e visitem todas as suas séries favoritas, tem o bastante para se divertir por um bom tempo."),
+      br(),
+      br(),
+      h4("Avaliação da série ao longo do tempo"),
+      plotlyOutput("line_graph"),
+      hr(),
+      hr(),
+      h4("Avaliação geral da série"),
+      plotlyOutput("dd")
+    )
+
+  )))
